d3e3bace45de763a0ced9ff7af216bb8ea9c3e47
Alexey <alex3122007@yandex.ru>
2023-01-16 23:12:02 +0300

    DZ

---
+++ 27/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int LengthArray(int n){
+    int count = 0;
+    for (int i = 1; i < n; i = i*10)
+         count ++;
+    return count;
+}
+
+void InputArray(int [] array,int n) {
+
+    for (int i = 0; i < array.Length ; i++){
+        array[i] = n%10;
+        n = n/10;
+    }
+
+}
+
+int Summ (int [] array){
+    int s =0;
+    for (int i = 0; i < array.Length ; i++){
+        s = s + array [i];
+    }
+    return s;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[LengthArray(n)];
+InputArray(array,n);
+Console.WriteLine($"= {Summ(array)}");
---
+++ 29/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов, которые вводит пользователь, и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+void input_array(int [] array)
+{
+
+    for (int i = 0; i < 8; i++){
+        Console.WriteLine($"Введите {i+1} из 8 элементов массива: ");   
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+    }
+}
+
+Console.Clear();
+int[] array = new int[8];
+input_array(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
